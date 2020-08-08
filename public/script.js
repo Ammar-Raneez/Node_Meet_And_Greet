3a9a9f40c5210db60ec40c56312c0e98168ed7f7
Ammar <--add>
2020-08-08 12:13:44 +0530

    Ports for Hosting

--- public/script.js
+++ public/script.js
@@ -3,7 +3,7 @@ const socket = io('/')
 var peer = new Peer(undefined, {
     path: '/peerjs',
     host: '/',
-    port: '3443'    //needed for hosting
+    port: '443'    //needed for hosting
 }); 
 
 //video element
